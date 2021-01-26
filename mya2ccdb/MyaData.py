1a5634f7be9c9b67ec082bc2fc1004717aeb2115
Adam Thornton <ademus4@gmail.com>
2021-01-26 15:35:06 -0500

    Get initial values in request, handle these to set first data value with all initial values

--- mya2ccdb/MyaData.py
+++ mya2ccdb/MyaData.py
@@ -15,11 +15,18 @@ class MyaPv:
       return self.name+','+str(self.deadband)
 
 class MyaDatum:
-  def __init__(self,datetime_str):
+  def __init__(self, datetime_str=None):
+    self.pvs={}
+    self.datetime=None
+    self.date=None
+    self.time=None
+    if datetime_str:
+      self.setTime(datetime_str)
+
+  def setTime(self, datetime_str):
     self.datetime = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S") 
     self.date = self.datetime.strftime("%Y-%m-%d")
     self.time = self.datetime.strftime("%H:%M:%S")
-    self.pvs={}
 
   def addPv(self,name,value):
     self.pvs[name]=value
@@ -28,6 +35,12 @@ class MyaDatum:
       return self.pvs[name]
     else:
       return None
+
+  def __str__(self):
+    s = "Date: {}\nTime: {}\n".format(self.date, self.time)
+    for pv in self.pvs:
+      s += "> pv: {}\tvalue: {}\n".format(pv, self.pvs[pv])
+    return s
 
 class MyaData:
   def __init__(self,start=None,end=None):
@@ -52,11 +65,16 @@ class MyaData:
   def get(self):
     data=[]
 
+    # create a empty myadatum which will hold the first value from each pv
+    data.append(MyaDatum())
+    print(data[0])
+
     for pv in self.pvs:
       params = {
         'b': self.start,
         'e': self.end,
-        'c': pv.name
+        'c': pv.name,
+        'p': 1  # prior point
       }
 
       # send get request to API for specific pv/channel
@@ -64,12 +82,25 @@ class MyaData:
 
       # iterate over the data in request, add a myadatum
       print(params['c'])
+      first = True
       for item in query.json()['data']:
         timestamp = item['d']
         value = item['v']
         md=MyaDatum(timestamp)
         md.addPv(pv.name, value)  # could add the validation here
-        data.append(md)
-    
+
+        if first:
+          print('First!')
+          print(pv.name, timestamp, value)
+          if not data[0].datetime:
+            data[0].setTime(timestamp)
+
+          data[0].addPv(pv.name, value)
+          
+          first = False
+        else:
+          data.append(md)
+
+    # first value in this list should have a data value for each pv
     data_sorted = sorted(data, key=attrgetter('datetime'))
     return data_sorted
